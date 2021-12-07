b6a594bc8b3d388a0dcc99433f215ada4b2e855d
Matheus D <matheus_cancao@hotmail.com>
2021-12-07 20:32:10 -0300

    change release name to 2.0.0-beta.1

--- unicocheck-ios/unicocheck_ios.h
+++ unicocheck-ios/unicocheck_ios.h
@@ -6,7 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import <AcessoBio/AcessoBioManagerDelegate.h>
 //! Project version number for unicocheck_ios.
 FOUNDATION_EXPORT double unicocheck_iosVersionNumber;
 
