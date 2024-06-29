45a56cba05bcbe87711d2cd37760d7aab9a4a0c4
Bruno Corrêa <113122540+bcorreau@users.noreply.github.com>
2024-06-29 13:56:01 -0300

    feat: iOS release 2.15.2 (#100)

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
             name: "unicocheck-ios-spm",
             targets: [
                 "AcessoBio",
+                "DeviceProfiling",
                 "FaceTecSDK",
                 "UnicoSdkBase",
                 "UnicoSdkLiveness",
@@ -26,6 +27,7 @@ let package = Package(
             resources: [.copy("PrivacyInfo.xcprivacy")]
         ),
         .binaryTarget(name: "AcessoBio",        path: "AcessoBio.xcframework"),
+        .binaryTarget(name: "DeviceProfiling",  path: "DeviceProfiling.xcframework"),
         .binaryTarget(name: "FaceTecSDK",       path: "FaceTecSDK.xcframework"),
         .binaryTarget(name: "UnicoSdkBase",     path: "UnicoSdkBase.xcframework"),
         .binaryTarget(name: "UnicoSdkLiveness", path: "UnicoSdkLiveness.xcframework"),
