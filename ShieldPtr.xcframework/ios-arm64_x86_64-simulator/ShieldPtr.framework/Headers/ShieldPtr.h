98818f990399d00fa6449f73c7998ddeff509f33
Bruno Corrêa <113122540+bcorreau@users.noreply.github.com>
2025-12-03 13:59:45 -0300

    feat: release 2.21.4 (#152)
    * feat: release 2.21.4
    * chore: updated Package.swift
    
    ---------
    
    Co-authored-by: Anka <runner@sat12-bq158-d75b1756-4de8-4e46-9ab8-74ae5446845d-AE53F5FFBFF4.local>

---
+++ ShieldPtr.xcframework/ios-arm64_x86_64-simulator/ShieldPtr.framework/Headers/ShieldPtr.h
@@ -0,0 +1,19 @@
+//
+//  ShieldPtr.h
+//  ShieldPtr
+//
+//  Created by Poe Poe Myint Swe on 9/1/20.
+//  Copyright © 2024 Shield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shield.
+FOUNDATION_EXPORT double ShieldPtrVersionNumber;
+
+//! Project version string for Shield.
+FOUNDATION_EXPORT const unsigned char ShieldPtrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShieldPtr/PublicHeader.h>
+
+
